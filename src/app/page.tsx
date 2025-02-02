c0139c00c7a00d9e36f40caac2e2f5d2608bc137
Anson Kao <anson.kao@gmail.com>
2025-02-01 19:50:33 -0500

    Renamed titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import Link from "next/link";
-
 import { LatestPost } from "~/app/_components/post";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 import { TranslationBox } from "./_components/TranslationBox";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
   const session = await auth();
 
   if (session?.user) {
@@ -17,10 +14,13 @@ export default async function Home() {
     <HydrateClient>
       <main className="flex min-h-screen flex-col bg-gradient-to-b from-gray-900 to-black text-white">
         <div className="container mx-auto flex flex-1 flex-col px-4 py-16">
-          <div className="mt-auto flex flex-col items-center gap-12">
+          <div className="mt-auto flex flex-col items-center gap-8">
             <h1 className="text-xl font-extrabold tracking-tight sm:text-[4rem]">
               English to <span className="text-blue-500">Chinese</span>
             </h1>
+            <p className="text-lg text-gray-400">
+              Speak Chinese by typing in English!
+            </p>
             <TranslationBox />
 
             {session?.user && <LatestPost />}
