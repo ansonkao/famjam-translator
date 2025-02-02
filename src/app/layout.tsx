ad7754751f0803df4e3f155dd41753597a866f25
Anson Kao <anson.kao@gmail.com>
2025-02-01 19:57:38 -0500

    Change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,16 +6,21 @@ import { type Metadata } from "next";
 import { TRPCReactProvider } from "~/trpc/react";
 
 export const metadata: Metadata = {
-  title: "FamJam Translator",
-  description: "Speak Chinese by typing in English",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  title: "FamJam",
+  description: "Speak Chinese by typing in English!",
+  icons: [
+    {
+      rel: "icon",
+      url: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>😀</text></svg>",
+    },
+  ],
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
+    <html lang="en" className={GeistSans.className}>
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
